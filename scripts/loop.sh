2dfaae8161fe488cf61a7655443837345661b063
ViskaWei <swei20@jhu.edu>
2020-10-19 22:42:50 -0400

    add norm selection

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -6,14 +6,28 @@ FTR=(rd src)
 NORM=(L T)
 # CONFIG=($TEST_CONFIG)
 
-
-for config in "${CONFIG[@]}"; do
-    for ftr in "${FTR[@]}"; do
-        for norm in "${NORM[@]}"; do
-            ./scripts/main.sh \
-                --config $config \
-                --ftr $ftr \
-                --norm $norm
+for norm in "${NORM[@]}"; do
+    if [ $norm == L ]
+    then
+        NORMDIM=(1 2)
+        p=elephant
+    elif [ $norm == T ]
+    then
+        NORMDIM=(8 16)
+        p=v100
+    else
+        raise error "norm funtion not recognized"
+    fi
+    for config in "${CONFIG[@]}"; do
+        for ftr in "${FTR[@]}"; do
+            for normDim in "${NORMDIM[@]}"; do
+                ./scripts/main.sh \
+                    sbatch -p $p\
+                    --config $config \
+                    --ftr $ftr \
+                    --norm $norm \
+                    --normDim $normDim
+            done
         done
     done
 done
